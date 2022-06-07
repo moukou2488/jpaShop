31a0c5d403694bfb06f10d0580083c9bceda6412
mouou2488 <flag326@hufs.ac.kr>
2022-06-07 20:03:11 +0900

    feat : item 비지니스로직

--- src/main/java/com/jpaproject/shop/domain/item/Item.java
+++ src/main/java/com/jpaproject/shop/domain/item/Item.java
@@ -1,6 +1,7 @@
 package com.jpaproject.shop.domain.item;
 
 import com.jpaproject.shop.domain.Category;
+import com.jpaproject.shop.exception.NotEnoughStockException;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.NoArgsConstructor;
@@ -25,4 +26,22 @@ public abstract class Item {
 
     @ManyToMany(mappedBy = "items")
     private List<Category> categories = new ArrayList<>();
+
+    //== 비지니스로직 ==
+
+    /**
+     * stock 증가
+     */
+    public void addStock(int quantity) {
+        this.stockQuantity += quantity;
+    }
+
+    public void removeStock(int quantity) {
+        int restStock = this.stockQuantity - quantity;
+
+        if(restStock < 0){
+            throw new NotEnoughStockException("need more stock"); }
+        this.stockQuantity = restStock;
+    }
+
 }
