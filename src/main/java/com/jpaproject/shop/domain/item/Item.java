b78b76c96f7d809db188ba554fbe91c660c935d0
mouou2488 <flag326@hufs.ac.kr>
2022-06-07 20:15:57 +0900

    feat : itemRepository 생성

--- src/main/java/com/jpaproject/shop/domain/item/Item.java
+++ src/main/java/com/jpaproject/shop/domain/item/Item.java
@@ -4,6 +4,7 @@ import com.jpaproject.shop.domain.Category;
 import com.jpaproject.shop.exception.NotEnoughStockException;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
+import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 import javax.persistence.*;
@@ -13,6 +14,7 @@ import java.util.List;
 @Entity
 @Inheritance(strategy = InheritanceType.SINGLE_TABLE)
 @DiscriminatorColumn(name = "dtype")
+@Getter
 public abstract class Item {
 
     @Id
