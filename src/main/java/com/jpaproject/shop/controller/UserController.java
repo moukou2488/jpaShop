9b8cb7440390a72ece80142da091c50026209be3
mouou2488 <flag326@hufs.ac.kr>
2022-06-09 19:39:34 +0900

    feat : 회원 등록, 조회 화면 및 컨트롤러

---
+++ src/main/java/com/jpaproject/shop/controller/UserController.java
@@ -0,0 +1,51 @@
+package com.jpaproject.shop.controller;
+
+import com.jpaproject.shop.domain.Address;
+import com.jpaproject.shop.domain.User;
+import com.jpaproject.shop.service.UserService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import javax.validation.Valid;
+import java.util.List;
+
+@Controller
+@RequiredArgsConstructor
+public class UserController {
+
+    private final UserService userService;
+
+    @GetMapping("/users/new")
+    public String createForm(Model model) {
+        model.addAttribute("userForm", new UserForm());
+        return "users/createUsersForm";
+    }
+
+    @PostMapping("/users/new")
+    public String create(@Valid UserForm userForm, BindingResult result) {
+
+        if (result.hasErrors()) {
+            return "users/createUsersForm";
+        }
+
+        Address address = new Address(userForm.getCity(), userForm.getStreet(), userForm.getZipcode());
+
+        User user = User.builder()
+                .name(userForm.getName())
+                .address(address)
+                .build();
+
+        userService.join(user);
+        return "redirect:/";
+    }
+
+    @GetMapping("/users")
+    public String list(Model model) {
+        model.addAttribute("users", userService.findUsers());
+        return "users/userList";
+    }
+}
---
+++ src/main/java/com/jpaproject/shop/controller/UserForm.java
@@ -0,0 +1,18 @@
+package com.jpaproject.shop.controller;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.validation.constraints.NotEmpty;
+
+@Getter
+@Setter
+public class UserForm {
+
+    @NotEmpty(message = "회원 이름은 필수 입니다.")
+    private String name;
+
+    private String city;
+    private String street;
+    private String zipcode;
+}
